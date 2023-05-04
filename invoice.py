fe79f12da0be685bb4fb3e3d691bdf83c64605aa
Ari-Ario <xusrew.z@gmail.com>
2023-05-04 18:37:57 +0200

    add-button and tree added

--- invoice.py
+++ invoice.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 class Invoice():
     def __init__(self, master):
@@ -22,6 +23,15 @@ class Invoice():
         self.entry_description= Entry(self.main_win)
         self.label_unitprice= Label(self.main_win, text="Unite Price")
         self.spinbox_unitprice= Spinbox(self.main_win, from_=0.0, to=1000, increment=0.5)
+        #button and treeview
+        self.button_add=Button(self.main_win, text="Add Item")
+        columns= ["quantity", "description", "price", "total"]
+        self.tree= ttk.Treeview(self.main_win, columns=columns, show="headings")
+        self.tree.heading("quantity", text="Quantity")
+        self.tree.heading("description", text="Description")
+        self.tree.heading("price", text="Price")
+        self.tree.heading("total", text="Total")
+
 
         self.main_win.pack(fill=BOTH, padx=10, pady=10)
         self.label_first_name.grid(row=0, column=0)
@@ -37,6 +47,9 @@ class Invoice():
         self.entry_description.grid(row=3, column=1)
         self.label_unitprice.grid(row=2, column=2)
         self.spinbox_unitprice.grid(row=3, column=2)
+        #grids of add-button and treeview
+        self.button_add.grid(row=4, column=2, padx=5, pady=5)
+        self.tree.grid(row=5, column=0, columnspan=3, padx=5, pady=10)
 
 if __name__ == "__main__":
     app = Tk()
